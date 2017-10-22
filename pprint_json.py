1af698131372149fc1c3953a519a9c73601bc125
Vitali Baranov <vitali.baranov@gmail.com>
2017-10-22 18:44:07 +0300

    comments were deleted, names of variables were changed

--- pprint_json.py
+++ pprint_json.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
-"""Read data from JSON file and print to stdout in a pretty manner"""
 import json
 import sys
 
 
-def load_data(filepath):
-    """Returns string loaded from JSON file"""
-    with open(filepath) as f:
-        return json.load(f)
+def load_json_data_from_file(filepath):
+    with open(filepath) as temp_file:
+        return json.load(temp_file)
 
 
-def pretty_print_json(data):
-    """Converts string data to JSON and prints it in a pretty manner"""
-    print(json.dumps(data, indent=2, ensure_ascii=False))
+def pretty_print_json_data(json_data):
+    print(json.dumps(json_data, indent=2, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        file_name = sys.argv[1]
+        input_file_name = sys.argv[1]
     else:
-        file_name = input('Input filepath: ')
-    pretty_print_json(load_data(file_name))
+        input_file_name = input('Input filepath: ')
+    json_data = load_json_data_from_file(input_file_name)
+    pretty_print_json_data(json_data)
     
